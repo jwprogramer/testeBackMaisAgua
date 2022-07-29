dc5c3c60d605eba2e82f11a082c8fdbf1ba11ec2
jwprogramer <johnwendel.wendel@gmail.com>
2022-07-28 22:09:04 -0300

    começou a brincadeira

---
+++ src/main/java/com/api/maisAgua/MaisAguaApplication.java
@@ -0,0 +1,13 @@
+package com.api.maisAgua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaisAguaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaisAguaApplication.class, args);
+	}
+
+}
